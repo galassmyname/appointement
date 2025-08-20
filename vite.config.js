832d2d11cfbf62c5a422da64450f12e6a1e69390
galassmyname <falloumyname@email.com>
2025-08-20 10:21:24 +0000

    a

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/css/filament/admin/theme.css'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: undefined,
-            },
-        },
-    },
 });
